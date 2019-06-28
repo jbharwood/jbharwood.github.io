d31cfe8d5015226811513ef72b9ac678a470857a
jbharwood <josephblakeharwood@gmail.com>
2019-06-27 21:13:46 -0400

    fixed scrolling up issue

--- jquery.js
+++ jquery.js
@@ -68,4 +68,26 @@ $(document).ready(function(){
       });
   });
 
+  function wheel(event) {
+      var delta = 0;
+      if (event.wheelDelta) {(delta = event.wheelDelta / 120);}
+      else if (event.detail) {(delta = -event.detail / 3);}
+
+      handle(delta);
+      if (event.preventDefault) {(event.preventDefault());}
+      event.returnValue = false;
+  }
+
+  function handle(delta) {
+      var time = 1000;
+      var distance = 300;
+
+      $('html, body').stop().animate({
+          scrollTop: $(window).scrollTop() - (distance * delta)
+      }, time );
+  }
+
+  if (window.addEventListener) {window.addEventListener('DOMMouseScroll', wheel, false);}
+    window.onmousewheel = document.onmousewheel = wheel;
+
 });
